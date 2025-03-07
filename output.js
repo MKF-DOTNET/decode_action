424c9afe816c8f6775e0393afe9c73a4aa2747b4
action <action@github.com>
2025-03-07 09:13:59 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Wed Mar 05 2025 10:42:09 GMT+0000 (Coordinated Universal Time)
+//Fri Mar 07 2025 09:13:59 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("潇洒桐庐");
